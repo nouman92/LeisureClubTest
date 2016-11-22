8851d71056e857ce2a0da2734590ad9944a598b1
muhammadbalal041 <muhammadbalal041@gmail.com>
2016-11-22 08:39:35 +0500

    Country is added with visible text United States

--- src/test/java/leisureclubtest/LeisureClubTestSuite.java
+++ src/test/java/leisureclubtest/LeisureClubTestSuite.java
@@ -43,28 +43,28 @@ public class LeisureClubTestSuite {
 	private WebDriver driver;
 
 	private void waitUntilSelectOptionsPopulated(final Select select) {
-        new FluentWait<WebDriver>(driver)
-                .withTimeout(60, TimeUnit.SECONDS)
-                .pollingEvery(10, TimeUnit.MILLISECONDS)
-                .until(new Predicate<WebDriver>() {
-                    public boolean apply(WebDriver d) {
-                        return (select.getOptions().size() > 1);
-                    }
-                });
-    }
-	
+		new FluentWait<WebDriver>(driver)
+		.withTimeout(60, TimeUnit.SECONDS)
+		.pollingEvery(10, TimeUnit.MILLISECONDS)
+		.until(new Predicate<WebDriver>() {
+			public boolean apply(WebDriver d) {
+				return (select.getOptions().size() > 1);
+			}
+		});
+	}
+
 	@BeforeTest
 	public void beforeTest() throws MalformedURLException {	
 
 		//PhantomJs Driver
-//		System.setProperty("phantomjs.binary.path", "phantomjs");
-//		String[] cli_args = new String[]{ "--ignore-ssl-errors=true" };
-//		DesiredCapabilities caps = DesiredCapabilities.phantomjs();
-//		caps.setCapability("takeScreenshot", "false");
-//		caps.setCapability( PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cli_args );
-//		caps.setCapability( PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
-//		driver =  new PhantomJSDriver( caps );
-		
+		//		System.setProperty("phantomjs.binary.path", "phantomjs");
+		//		String[] cli_args = new String[]{ "--ignore-ssl-errors=true" };
+		//		DesiredCapabilities caps = DesiredCapabilities.phantomjs();
+		//		caps.setCapability("takeScreenshot", "false");
+		//		caps.setCapability( PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cli_args );
+		//		caps.setCapability( PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
+		//		driver =  new PhantomJSDriver( caps );
+
 		//chrome remote Driver
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		ChromeOptions options = new ChromeOptions();
@@ -72,7 +72,7 @@ public class LeisureClubTestSuite {
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 		driver = new ChromeDriver(capabilities);
-		
+
 		//driver.manage().window().setSize(new Dimension(1920,1080));
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
@@ -179,46 +179,46 @@ public class LeisureClubTestSuite {
 			WebDriverWait WaitFirstName = new WebDriverWait(driver, 100);
 			WaitFirstName.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:firstname']"))).sendKeys("test");
 			System.out.println("First Name is Enterd");
-			
+
 			WebDriverWait WaitLastName = new WebDriverWait(driver, 100);
 			WaitLastName.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:lastname']"))).sendKeys("test");
 			System.out.println("Last Name is Enterd");
-			
+
 			//*[@id="billing:email"]
 			WebDriverWait WaitEmail = new WebDriverWait(driver, 100);
 			WaitEmail.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:email']"))).sendKeys("test@shopistan.pk");
 			System.out.println("Email is Enterd");
-			
+
 			//*[@id="billing:confirm_email"]
 			WebDriverWait WaitConfirmEmail = new WebDriverWait(driver, 100);
 			WaitConfirmEmail.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:confirm_email']"))).sendKeys("test@shopistan.pk");
 			System.out.println("Email is confirmed");
-			
+
 			//*[@id="billing:street1"]
 			WebDriverWait WaitStreet1 = new WebDriverWait(driver, 100);
 			WaitStreet1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:street1']"))).sendKeys("test");
 			System.out.println("Street 1 is Enterd");
-			
+
 			//*[@id="billing:street2"]
 			WebDriverWait WaitStreet2 = new WebDriverWait(driver, 100);
 			WaitStreet2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:street2']"))).sendKeys("test");
 			System.out.println("Street2 is Enterd");
-			
+
+			//*[@id="billing:country_id"]
+			Select oSelect2 = new Select(driver.findElement(By.xpath("//*[@id='billing:country_id']")));
+			//waitUntilSelectOptionsPopulated(oSelect2);
+			oSelect2.selectByVisibleText("United States");
+
 			WebDriverWait WaitPostCode = new WebDriverWait(driver, 100);
 			WaitPostCode.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='billing:postcode']"))).sendKeys("test");
 			System.out.println("Billing postcode is Enterd");
-			
-			//*[@id="billing:country_id"]
-//			Select oSelect2 = new Select(driver.findElement(By.xpath("//*[@id='billing:country_id']")));
-//			waitUntilSelectOptionsPopulated(oSelect2);
-//			oSelect2.selectByVisibleText("PAKISTAN");
 
 			//*[@id="billing:city"]
 			//Select oSelect3 = new Select(driver.findElement(By.xpath("html/body/div[1]/div/div[5]/div/div/div[1]/div[5]/form[1]/div/ul/li[1]/fieldset/ul/li[7]/div[1]/div[1]/div/select")));
 			//Select oSelect3 = new Select(driver.findElement(By.name("billing[city]")));
-//			Select oSelect3 = new Select(driver.findElement(By.xpath("//*[@id='billing:city']")));
-//			waitUntilSelectOptionsPopulated(oSelect3);
-//			oSelect3.selectByIndex(3);
+			//			Select oSelect3 = new Select(driver.findElement(By.xpath("//*[@id='billing:city']")));
+			//			waitUntilSelectOptionsPopulated(oSelect3);
+			//			oSelect3.selectByIndex(3);
 
 			//For Server Machine
 			driver.findElement(By.xpath("//*[@id='billing:city']")).sendKeys("Texas");;
@@ -228,17 +228,17 @@ public class LeisureClubTestSuite {
 			Select oSelect3 = new Select(driver.findElement(By.xpath("//*[@id='billing:region_id']")));
 			waitUntilSelectOptionsPopulated(oSelect3);
 			oSelect3.selectByIndex(1);
-			
+
 			//*[@id="billing:telephone"]
 			driver.findElement(By.xpath("//*[@id='billing:telephone']")).sendKeys("03001234567");
 			System.out.println("Telephone is Enterd");
 			//*[@id="tel2"]
-//			driver.findElement(By.xpath("//*[@id='tel2']")).sendKeys("03001234567");
-//
-//
-//
-//			//SELECT CASH ON DELEIVERY
-//			driver.findElement(By.id("p_method_cashondelivery")).click();
+			//			driver.findElement(By.xpath("//*[@id='tel2']")).sendKeys("03001234567");
+			//
+			//
+			//
+			//			//SELECT CASH ON DELEIVERY
+			//			driver.findElement(By.id("p_method_cashondelivery")).click();
 
 			//PLACE ORDER
 			WebDriverWait wait3 = new WebDriverWait(driver, 200);
